dea4bc6b2696c351786bf66c0828f35b1df9eae8
Maxim Samoilov <samoilowmaxim@gmail.com>
2017-01-05 11:46:10 +0300

    Add driver

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import xs, { Stream } from 'xstream'
+
+export type MiddlewareInput = any
+export type Middleware<MI, A> = (middlewareInput: MI) => Stream<A>
+export type Reducer<S, A> = (state: S, action: A) => S
+
+export class StateSource<State, Action, Actions> {
+  public readonly $: Stream<State>
+  public readonly actions: Actions
+
+  constructor(
+    initialState: State,
+    actions: Actions,
+    reducer: (state: State, action: Action) => State,
+    middleware: Middleware<MiddlewareInput, Action>,
+    actions$: Stream<MiddlewareInput>,
+  ) {
+    this.actions = actions
+    this.$ = actions$
+      .map(middleware)
+      .flatten()
+      .fold(reducer, initialState)
+  }
+}
+
+export function makeStateDriver<State, Action, Actions>(
+  initialState: State,
+  actions: Actions,
+  reducer: Reducer<State, Action>,
+  middleware: Middleware<MiddlewareInput, Action> = (action: Action) => xs.of(action),
+) {
+  return function stateDriver(actions$: Stream<MiddlewareInput>) {
+    return new StateSource(initialState, actions, reducer, middleware, actions$)
+  }
+}
+ 
